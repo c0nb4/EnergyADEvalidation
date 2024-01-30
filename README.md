48ce2fd62d29056e95820b6edf048b9e68908eb3
fr1303 <rehmann@tu-berlin.de>
2024-01-30 11:02:35 +0800

    schema parser

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # EnergyADEvalidation
-Python Script to validate CityGML + Energy ADE Files.
+Python Script to validate CityGML + Energy ADE Files. Collection of necessary schemas is provided, to reduce effor to gather and adjust the necessary files. 
 
 Requires lxml. 
 
@@ -8,7 +8,7 @@ Requires lxml.
 
 Example data is given in the (data folder)[/data]. The data is gathered from (CityGML Wiki)[https://www.citygmlwiki.org/index.php?title=Citygml_Wiki]
 
-# Common Errors
+# Common Errors in cityGML files
 
 - uom (unit of measurment) is missing. 
 - Booleans, e.g. IsMechnicallyVentilated == True are missing. 
--- validate.py
+++ validate.py
@@ -10,6 +10,7 @@ def validate(xmlfile, xsdfile):
     xml_doc = etree.parse(xmlfile)
     # result = xmlschema.validate(xml_doc)
     try:
+        # Returns None when the document is valid 
         result = xmlschema.assertValid(xml_doc)
         return result 
     except etree.XMLSchemaError as e:
@@ -19,28 +20,63 @@ def validate(xmlfile, xsdfile):
     except Exception as e:
         return f"Error: {e}"
 
-    return result
-
-""" 
 
 def get_xsd(gml_file):
-    
-    Get the XSD file for the given GML file.
+    """
+    Get the XSD file for the given GML file.    
+    """
     
     parser = etree.XMLParser(ns_clean=True)
     try:
-        doc = etree.parse(fIn)    
+        doc = etree.parse(gml_file)
     except:
         doc = None
         xsd = None
         return doc, xsd   
-    root = doc.getroot()
-    # 
+    root = doc.getroot() 
+    energy_ade_version = ""
+    citygmlversion = "" 
+    for key in root.nsmap.keys():
+        if root.nsmap[key].find('www.opengis.net/citygml') != -1:
+                if (root.nsmap[key][-3:] == '1.0'):
+                    citygmlversion = '1.0'
+                    break
+                if (root.nsmap[key][-3:] == '2.0'):
+                    citygmlversion = '2.0'
+                    break
+    for key in root.nsmap.keys():
+        if root.nsmap[key].find('www.sig3d.org/citygml/2.0/energy') != -1:
+            if (root.nsmap[key][-3:] == '1.0'):
+                    energy_ade_version = '1.0'
+                    break
+            if (root.nsmap[key][-3:] == '2.0'):
+                    energy_ade_version = '2.0'
+                    break 
+    if citygmlversion == "":
+        return None, None
+    elif citygmlversion == "2.0" and energy_ade_version == "":
+        xsd = etree.parse("schemas/CityGML2.0/CityGML.xsd")
+    elif citygmlversion == "2.0" and energy_ade_version == "1.0":
+        xsd = etree.parse("schemas/CityGML2.0_EnergyADE1.0/CityGML.xsd")
+    elif citygmlversion == "2.0" and energy_ade_version == "2.0":
+        xsd = etree.parse("schemas/CityGML2.0_EnergyADE2.0/CityGML.xsd")
+    else:
+        return None, None
+    return doc, xsd
+    
 
-""" 
 
 
 if __name__ == "__main__":
 
-    result = validate(r'C:\Users\felix\Programmieren\EnergyADEvalidation\data\FZKHouseLoD2.gml', r'C:\Users\felix\Programmieren\EnergyADEvalidation\schemas\CityGML2.0\CityGML.xsd')
+    gml_path = r'C:\Users\felix\Programmieren\EnergyADEvalidation\data\EnergyADE2.0\ASHRAE_140_610.gml'
+    xsd_path = r'C:\Users\felix\Programmieren\EnergyADEvalidation\schemas\CityGML2.0\CityGML.xsd' 
+
+    #result = validate(gml_path, xsd_path)
+    #print(result)
+    
+    doc, xsd =  get_xsd(gml_file=gml_path)
+    print(doc, xsd )
+
+    result = validate(gml_path, xsd_path)
     print(result)
